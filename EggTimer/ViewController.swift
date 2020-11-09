baabddd4a19ad3a52968898f343a91a484a47c9d
MoBoustta <moboustta6@gmail.com>
2020-11-09 14:13:28 +0100

    add a progress view

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,37 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = [ "Soft": 3, "Medium": 4, "Hard": 720 ]
+    let eggTimes = [ "Soft": 5, "Medium": 420, "Hard": 720 ]
     
-    var secondsRemaining = 59
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressBar.progress = 0
+    }
+    
+    fileprivate func reset(_ hardness: String) {
+        timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        topLabel.text = hardness
+    }
+    
+    fileprivate func playSound(){
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
-        timer.invalidate()
         let hardness = sender.currentTitle!
+        reset(hardness)
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
-    if secondsRemaining > 0 {
-        print(secondsRemaining)
-        secondsRemaining -= 1
-        } else {
-        timer.invalidate()
-        topLabel.text = "Done!"
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        }
+        else {
+            timer.invalidate()
+            topLabel.text = "Done!"
+            playSound()
         }
     }
     
